aa29239fcb3d7b1285f44b244eee25298ca526c3
TB831 <druu831@gmail.com>
2019-07-10 12:10:04 -0700

    Function completed: fibonacci

--- fibonacci/index.js
+++ fibonacci/index.js
@@ -1,44 +1,28 @@
-function fib(n) { // Basic Recursion
-  if (n === 2) {
-    return 1;
+// Leetcode: https://leetcode.com/problems/fibonacci-number/
+// Description: The Fibonacci numbers, commonly denoted F(n) form a sequence, 
+// called the Fibonacci sequence, such that each number is the sum of the 
+// two preceding ones, starting from 0 and 1. That is,
+// F(0) = 0,   F(1) = 1
+// F(N) = F(N - 1) + F(N - 2), for N > 1.
+
+// Given N, calculate F(N).
+
+// Example 1:
+// Input: 2
+// Output: 1
+// Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+function fib(N) { // Basic Recursion
+  if (N < 2) {
+    return N;
   }
-  else if (n === 1) {
-    return 0;
-  }
-  return fib(n-1) + fib(n-2);
+  return fib(N-1) + fib(N-2);
 }
 
-console.log(fib(5)); // 3
-console.log(fib(7)); // 8
-console.log(fib(10)); // 34
-console.log(fib(17)); // 987
-console.log();
-
-function fibWithTab(n) { // Fib with tabulation
+function fibWithTab(N) { // Fib with tabulation
   let tab = [0,1];
-  for (let i=2; i<=n; i++) {
+  for (let i=2; i<=N; i++) {
       tab.push(tab[i-1] + tab[i-2]);
   }
-  return tab[n];
+  return tab[N];
 }
-
-console.log(fibWithTab(5)); // 3
-console.log(fibWithTab(7)); // 8
-console.log(fibWithTab(10)); // 34
-console.log(fibWithTab(17)); // 987
-console.log();
-
-function fibWithMemoize(n, memo={1:0, 2:1}) {
-  if (n in memo) {
-    return memo[n];
-  } else {
-    memo[n] = fibWithMemoize(n-1, memo) + fibWithMemoize(n-2, memo);
-    return memo[n];
-  }
-}
-
-console.log(fibWithMemoize(5)); // 3
-console.log(fibWithMemoize(7)); // 8
-console.log(fibWithMemoize(10)); // 34
-console.log(fibWithMemoize(17)); // 987
-console.log();
