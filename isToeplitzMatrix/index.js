d19072b0069b2b39c0d1a48e5c8e1a6a5813c30c
TB831 <druu831@gmail.com>
2019-07-16 16:56:46 -0700

    Function completed: isToeplitzMatrix

---
+++ isToeplitzMatrix/index.js
@@ -0,0 +1,26 @@
+// Leetcode: https://leetcode.com/problems/toeplitz-matrix/
+// Description: A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same element.
+// Now given an M x N matrix, return True if and only if the matrix is Toeplitz.
+
+// Example 1:
+// Input:
+// matrix = [
+//   [1,2,3,4],
+//   [5,1,2,3],
+//   [9,5,1,2]
+// ]
+
+// Output: True
+// Explanation:
+// In the above grid, the diagonals are:
+// "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+// In each diagonal all elements are the same, so the answer is True.
+
+function isToeplitzMatrix(matrix) {
+  for (let i=0; i<matrix.length-1; i++) {
+    for (let j=1; j<matrix[i].length-1; j++) {
+      if (matrix[i][j] !== matrix[i+1][j+1]) return false;
+    }
+  }
+  return true;
+}
